4ad4d5facb64ca9a28d005f885cb798d731ce00f
fruiz10code <93126000+fruiz10code@users.noreply.github.com>
2022-09-14 12:50:34 +0200

    add vi tournament

--- constants/data.js
+++ constants/data.js
@@ -3,6 +3,29 @@ import {goToBlankLink} from "../lib/utils";
 
 const data = [
   {
+    title: "6º Open Pata Negra",
+    text: "Durante los próximos días 23,24 y 25 de septiembre se celebra el 6º Open Pata Negra, puede consultar el cartel y los cuadros en el botón más abajo",
+    image: "/cartel_6_torneo.jpg",
+    imageAlt: "VI Torneo Los Pata Negra",
+    actions: [
+      {
+        onClick: ({location}) =>
+          goToBlankLink(`${location}/cartel_6_torneo.jpg`),
+        label: "Consultar cartel",
+        color: "danger",
+      },
+      {
+        onClick: () =>
+          goToBlankLink(
+            "https://docs.google.com/spreadsheets/d/1tLXYfBDdVjAFoijf7rQeiPC_gBjmmAmo"
+          ),
+        label: "Consultar cuadros",
+        color: "primary",
+        disabled: true,
+      },
+    ],
+  },
+  {
     title: "5º Open Pata Negra",
     text: "Durante los próximos días 24,25 y 26 de septiembre se celebra el 5º Open Pata Negra, puede consultar el cartel y los cuadros en el botón más abajo",
     image: "/cartel_5_torneo.jpg",
